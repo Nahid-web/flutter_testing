29e5ccde4d4471e85d5952dfa6e581f65390af5d
Nahid-web <nahidamin266@gmail.com>
2025-04-11 21:11:12 +0600

    Refactors app to fetch and display user data
    Converts counter demo app into a user data display application
    - Integrates HTTP client for API calls
    - Adds user model and repository
    - Implements FutureBuilder for async data handling
    - Removes counter-related functionality

--- unit_test/users_http_example/lib/main.dart
+++ unit_test/users_http_example/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:users_http_example/user_model.dart';
+import 'package:users_http_example/user_repository.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Unit Testing',
+
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
@@ -29,37 +34,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  Future<User> getUsers = UserRepository(client: Client()).fetchUser();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      appBar: AppBar(title: Text(widget.title)),
+      body: FutureBuilder(
+        future: getUsers,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return Center(child: Text(snapshot.data!.name));
+        },
       ),
     );
   }
